ff2dd721b0e745774bedf5402f3944c19808f2e7
Gerardo Santos <grnsantos899@gmail.com>
2025-12-03 21:09:43 -0600

    Diseño finalizado index

---
+++ includes/card.php
@@ -0,0 +1,10 @@
+<div class="card-container">
+    <figure class="card-image">
+        <!-- <img src="../assets/img/apple-iphone-16-plus.png" alt="product"> -->
+        <div class="image-product"></div>
+    </figure>
+    <div class="card-info">
+        <p>iPhone 16</p>
+        <p class="price">13,999 MXN</p>
+    </div>
+</div>
